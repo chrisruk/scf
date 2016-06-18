63afe72edefcc9f6864a7edd0d310e0f09398066
chrisruk <chrisrichardsonuk@gmail.com>
2016-06-18 21:42:51 +0100

    tidy

--- scf.py
+++ scf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import matplotlib.pyplot as plot
 import numpy as np
 import sys
@@ -14,39 +13,20 @@ import numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-
 from itertools import islice
 
-def window(seq, n=2):
-    "Returns a sliding window (of width n) over data from the iterable"
-    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
-    it = iter(seq)
-    result = tuple(islice(it, n))
-    if len(result) == n:
-        yield result    
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
-
-
-
-
-
 za = []
-#y = np.fromfile("/tmp/out.dat", dtype=np.complex64)
-y = np.fromfile("/tmp/out2.dat", dtype=np.float32)
+y = np.fromfile("/tmp/out.dat", dtype=np.complex64)
 plt.plot(y)
 plt.show()
-#quit()
-d = collections.deque(maxlen=5)
 
-# Number of frames
-N = 140
-T = int(len(y) / N)
-print("Framelen ",T)
+d = collections.deque(maxlen=10)
 
-Fs = T
-al = 1*(Fs*2)
+N = 100             # Number of frames
+T = int(len(y) / N) # Frame length
+
+Fs = T*2
+al = 1*Fs
 n = 0
 
 frame = y[(n*int(T)):int(n*T)+int(T)]
@@ -66,21 +46,17 @@ alph = []
 for a in np.arange(0,1,0.05):
 
 
-    Fs = T #*2
-    al = a * (Fs*2)
+    Fs = T*2
+    al = a * Fs
     alph.append(a)
     print("Alph",al)
     
-    T = int(len(y) / N)
-    print("Tlen",T)
-
     out = []
 
     count = 0
     for n in range(0,N):
-    #for frame in window(y,T):
+
         count = n
-        #print("v",count/len(y),len(frame))
         frame = y[int(n*T):int(n*T)+T]
         print("Frame ",int(n*T),int(n*T)+T)
         xf = np.fft.fftshift(np.fft.fft(frame))
@@ -88,7 +64,8 @@ for a in np.arange(0,1,0.05):
         xfm = np.append(xf,[0]*int(al/2))
         np.set_printoptions(threshold=np.nan)
         
-        Sxf = ((1/T) * xfp * np.conj(xfm)) * (np.e**-(1j*2*np.pi*al*(count*T)))
+        Sxf = (1/T) * xfp * np.conj(xfm) 
+        Sxf = Sxf * (np.e**-(1j*2*np.pi*al*(count*T)))
     
         orig = len(Sxf)
         Sxf.resize((mx,))
@@ -101,7 +78,7 @@ for a in np.arange(0,1,0.05):
             new.append(math.sqrt(v.imag**2+v.real**2))
     
         out.append(new)
-    #tm = out[0]
+    
     tm = np.mean( np.array(out), axis=0 )
     d.append(tm)
 
@@ -109,31 +86,24 @@ for a in np.arange(0,1,0.05):
     smoothed = np.mean(np.array(d),axis=0)
     za.append(smoothed)
         
-# Set up grid and test data
 
 za = np.array(za)
 nx, ny = za.shape[1], za.shape[0]
-x = range(nx)
+x = np.arange(0.5,-0.5,-1/mx)
 y = alph
-#print("X",x)
-#y = range(ny)
 
 hf = plt.figure()
 ha = hf.add_subplot(111, projection='3d')
 
 ha.set_xlabel('Frequency')
 ha.set_ylabel('Alpha')
-ha.set_zlabel('SCF (Need to normalise)')
+ha.set_zlabel('SCF')
 
-X, Y = numpy.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+X, Y = numpy.meshgrid(x, y) 
 
 ha.plot_surface(X, Y, za,rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-#ha.plot_wireframe(X,Y,za)
 plt.show()
-
 quit()
-
-
 
 print("Shape ",np.array(za).shape)
 plot.imshow(za,aspect='auto' ,cmap='hot')
