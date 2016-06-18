ab5ded680b7c1206db4d7c4a1dd12b54a2946a0e
chrisruk <chrisrichardsonuk@gmail.com>
2016-06-18 16:40:09 +0100

    float reading

--- scf.py
+++ scf.py
@@ -15,27 +15,46 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-Fs = 32000;  # sampling rate
-Ts = 1.0/Fs; # sampling interval
+from itertools import islice
+
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+
+
 
 
 za = []
-y = np.fromfile("/tmp/out.dat", dtype=np.complex64)
-
+#y = np.fromfile("/tmp/out.dat", dtype=np.complex64)
+y = np.fromfile("/tmp/out2.dat", dtype=np.float32)
+plt.plot(y)
+plt.show()
+#quit()
 d = collections.deque(maxlen=5)
 
 # Number of frames
-N = 100
+N = 140
+T = int(len(y) / N)
+print("Framelen ",T)
 
-al = 1*Fs
+Fs = T
+al = 1*(Fs*2)
 n = 0
 
-T = int(len(y) / N)
 frame = y[(n*int(T)):int(n*T)+int(T)]
 xf = np.fft.fftshift(np.fft.fft(frame))
 xfp = np.append([0]*int(al/2),xf)
 xfm = np.append(xf,[0]*int(al/2))
 Sxf = (1/T) * xfp * np.conj(xfm)
+Sxf = Sxf * (np.e**-(1j*2*np.pi*al*(N*T)))
 
 mx = len(Sxf)
 
@@ -44,27 +63,32 @@ print("Mx",mx)
 
 alph = []
 
-for a in range(0,20,1):
-    al = (a/20.0) * Fs
-    alph.append(a/20.0)
+for a in np.arange(0,1,0.05):
+
+
+    Fs = T #*2
+    al = a * (Fs*2)
+    alph.append(a)
     print("Alph",al)
     
     T = int(len(y) / N)
     print("Tlen",T)
 
     out = []
+
+    count = 0
     for n in range(0,N):
     #for frame in window(y,T):
-        frame = y[(n*int(T)):int(n*T)+T]
+        count = n
+        #print("v",count/len(y),len(frame))
+        frame = y[int(n*T):int(n*T)+T]
+        print("Frame ",int(n*T),int(n*T)+T)
         xf = np.fft.fftshift(np.fft.fft(frame))
-
-
         xfp = np.append([0]*int(al/2),xf)
         xfm = np.append(xf,[0]*int(al/2))
         np.set_printoptions(threshold=np.nan)
         
-        Sxf = (1/T) * xfp * np.conj(xfm)
-        #Sxf = xfp * np.conj(xfm)
+        Sxf = ((1/T) * xfp * np.conj(xfm)) * (np.e**-(1j*2*np.pi*al*(count*T)))
     
         orig = len(Sxf)
         Sxf.resize((mx,))
