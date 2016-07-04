948e770d39921ec30ead6d00dca5d0f6bae581e8
chrisruk <chrisrichardsonuk@gmail.com>
2016-07-04 16:52:56 +0100

    tidy

--- tensor_ann_load.py
+++ tensor_ann_load.py
@@ -5,12 +5,6 @@ import numpy as np
 from tensor import *
 
 input_data = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]  # XOR input
-#output_data = [[0.], [1.], [1.], [0.]]  # XOR output
-
-temp = "/tmp/"
-output_graph_name = "output_graph.pb"
-output_graph_path = os.path.join(temp, output_graph_name)
-
-sess, inp, out = load_graph(output_graph_path)
+sess, inp, out = load_graph("/tmp/output_graph.pb")
 
 print(sess.run(out,feed_dict={inp: [[0., 1. ]]}))
